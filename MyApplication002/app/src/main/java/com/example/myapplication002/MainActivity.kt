9fe140ad2a87200d3da742396a51646bcdb235e5
FilipVit1903 <vit.filip1903@gmail.com>
2024-09-28 23:03:00 +0200

    #2 login_app_done

--- MyApplication002/app/src/main/java/com/example/myapplication002/MainActivity.kt
+++ MyApplication002/app/src/main/java/com/example/myapplication002/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.myapplication002
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +25,28 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         */
+
+        //Definice prvku
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val validUsername = findViewById<TextView>(R.id.validUsername)
+        val validPassword = findViewById<TextView>(R.id.validPassword)
+        val twSuccess = findViewById<TextView>(R.id.twSuccess)
+        val twFail = findViewById<TextView>(R.id.twFail)
+
+        btnLogin.setOnClickListener {
+            val Username = etUsername.text.toString()
+            val Password = etPassword.text.toString()
+
+            if (Username == validUsername.text && Password == validPassword.text ) {
+                twSuccess.visibility = View.VISIBLE
+                twFail.visibility = View.GONE
+            } else {
+                twFail.visibility = View.VISIBLE
+                twSuccess.visibility = View.GONE
+            }
+        }
+
     }
 }

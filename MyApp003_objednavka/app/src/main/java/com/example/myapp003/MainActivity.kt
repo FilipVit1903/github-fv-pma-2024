e7d123c4bf6385659e483b04a1ceb9bf296af0de
FilipVit1903 <vit.filip1903@gmail.com>
2024-10-03 15:36:07 +0200

    #3 img change with radioButtons

--- MyApp003_objednavka/app/src/main/java/com/example/myapp003/MainActivity.kt
+++ MyApp003_objednavka/app/src/main/java/com/example/myapp003/MainActivity.kt
@@ -1,25 +1,61 @@
 package com.example.myapp003
 
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp003.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
+        //binding settings
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-         */
-
+        //Change title
         title = "Objednávka smoothie"
+
+        binding.btnOrder.setOnClickListener {
+            //id load radiobutton from radioGroup
+            val smoothieId = binding.rgSmoothies.checkedRadioButtonId
+
+            val smoothie = findViewById<RadioButton>(smoothieId)
+
+            val fruit = binding.cbFruit.isChecked
+            val milk = binding.cbMilk.isChecked
+            val ice = binding.cbIce.isChecked
+
+            val orderText = "Souhrn objednávky: " +
+                    "${smoothie.text}" +
+                    (if (fruit) "; ovoce navíc" else "") +
+                    (if (milk) "; mléko navíc" else "") +
+                    (if (ice) ";  s ledem" else "")
+
+            binding.tvOrder.text = orderText
+        }
+
+        //Image change on choose radioButton
+        binding.rbSmoothie01.setOnClickListener {
+            binding.imgSmoothie.setImageResource(R.drawable.strawberry_smoothie)
+        }
+
+        binding.rbSmoothie02.setOnClickListener {
+            binding.imgSmoothie.setImageResource(R.drawable.pineaple_smoothie)
+        }
+
+        binding.rbSmoothie03.setOnClickListener {
+            binding.imgSmoothie.setImageResource(R.drawable.peach_smoothie)
+        }
+
     }
 }

6d856de6dbeb6b516e6f342d9bd38c4f5f1ccbbe
FilipVit1903 <vit.filip1903@gmail.com>
2024-10-10 15:25:05 +0200

    #5 prepnuti a natahnuti do druhe aktivity

---
+++ MyApp005_MoreActivities/app/src/main/java/com/example/myapp005_moreactivities/SecondActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapp005_moreactivities
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+        val twInfo = findViewById<TextView>(R.id.twInfo)
+
+        //Load data from intent
+        val nickname = intent.getStringExtra("NICK_NAME")
+        twInfo.text = "Data z první aktivity Jméno: $nickname"
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+
+        val btnThird = findViewById<Button>(R.id.btnThird)
+    }
+}

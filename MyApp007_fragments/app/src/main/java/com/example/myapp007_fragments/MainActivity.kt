d1d1026c6593414943e4b4d010fb1f98cbe0da86
FilipVit1903 <vit.filip1903@gmail.com>
2024-10-24 11:55:51 +0200

    #7 fragment wrks

--- MyApp007_fragments/app/src/main/java/com/example/myapp007_fragments/MainActivity.kt
+++ MyApp007_fragments/app/src/main/java/com/example/myapp007_fragments/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.myapp007_fragments
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.myapp007_fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        lateinit var binding: ActivityMainBinding
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFragment01.setOnClickListener {
+            replaceFragment(Fragment01())
         }
+
+        binding.btnFragment02.setOnClickListener {
+            replaceFragment(Fragment02())
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        //get instance from fragment manager
+        val fragmentManager = supportFragmentManager
+
+        //create transaction
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.fragContainer, fragment)
+
+        fragmentTransaction.commit()
+    }
+
 }

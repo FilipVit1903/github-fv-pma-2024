b1e44a990d402cf3a50aafad7acb120d36061d15
FilipVit1903 <vit.filip1903@gmail.com>
2024-10-24 11:56:36 +0200

    #6 WIP

--- MyApp006_toast_snackbar/app/src/main/java/com/example/myapp006_toast_snackbar/MainActivity.kt
+++ MyApp006_toast_snackbar/app/src/main/java/com/example/myapp006_toast_snackbar/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.example.myapp006_toast_snackbar
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp006_toast_snackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Nastaveni akce pro btn
+        binding.btnShowToast.setOnClickListener {
+            val toast = Toast.makeText(this, "Hello - I´m hungry", Toast.LENGTH_LONG)
+            toast.show()
         }
+
+        //Snackbar
+        binding.btnShowSnackbar.setOnClickListener {
+            val snackbar = Snackbar.make(binding.root, "Snackbar text", Snackbar.LENGTH_LONG)
+            snackbar.show()
+            snackbar.setDuration(7000).setBackgroundTint(Color.parseColor("#547465"))
+                .setActionTextColor(Color.parseColor("red"))
+
+            snackbar.setAction("Zavřít") {
+                Toast.makeText(this, "Zavírám SNACK", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        //Custom TOAST
+        binding.btnShowCustomToast.setOnClickListener {
+            //nacteni custom layoutu toast
+            val toastLayout = layoutInflater.inflate(R.layout.custom_toast_layout)
+
+        }
+
+
     }
+}
+
+
+}
+
+
+
 }

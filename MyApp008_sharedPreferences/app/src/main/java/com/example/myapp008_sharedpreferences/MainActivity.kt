2bd24fed38326dc17b942b89895ff52a58cbe572
FilipVit1903 <vit.filip1903@gmail.com>
2024-10-24 15:32:15 +0200

    #8 app done

--- MyApp008_sharedPreferences/app/src/main/java/com/example/myapp008_sharedpreferences/MainActivity.kt
+++ MyApp008_sharedPreferences/app/src/main/java/com/example/myapp008_sharedpreferences/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.example.myapp008_sharedpreferences
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp008_sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Přístup k SharedPreferences
+        val sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        binding.btn1.setOnClickListener {
+            val name = binding.etName.text.toString()
+            val ageString = binding.etAge.text.toString().trim()
+
+            if (ageString.isBlank()) {
+                Toast.makeText(this, "Věk je prázdný", Toast.LENGTH_LONG).show()
+            } else {
+                val age = ageString.toInt()
+                val isAdult = binding.chbAdult.isChecked
+                if ((age < 18 && isAdult) || (age >= 18 && !isAdult)) {
+                    Toast.makeText(this, "Není ti 18 nebo více", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "V pořádku", Toast.LENGTH_LONG).show()
+                    editor.apply {
+                        putString("name", name)
+                        putInt("age", age)
+                        putBoolean("isAdult", isAdult)
+                        apply()
+                    }
+                }
+            }
         }
+
+        // Funkcionalita pro načtení dat
+        binding.btn2.setOnClickListener {
+            val name = sharedPref.getString("name", null)
+            val age = sharedPref.getInt("age", 0)
+            val isAdult = sharedPref.getBoolean("isAdult", false)
+
+            binding.etName.setText(name)
+            binding.etAge.setText(age.toString())
+            binding.chbAdult.isChecked = isAdult
+        }
+
     }
 }

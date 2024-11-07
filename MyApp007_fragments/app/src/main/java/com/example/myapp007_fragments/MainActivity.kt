ad5d513a7d59c178b54751c31a0a978dfd26b707
FilipVit1903 <vit.filip1903@gmail.com>
2024-11-07 10:54:25 +0100

    #7 commit

--- MyApp007_fragments/app/src/main/java/com/example/myapp007_fragments/MainActivity.kt
+++ MyApp007_fragments/app/src/main/java/com/example/myapp007_fragments/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.myapp007_fragments.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 

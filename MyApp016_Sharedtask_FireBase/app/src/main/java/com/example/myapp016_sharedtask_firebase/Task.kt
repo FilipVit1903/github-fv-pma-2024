4a9c43e6f211968729afa7f2731b38d70e115b78
FilipVit1903 <vit.filip1903@gmail.com>
2024-12-05 11:35:58 +0100

    #16 init

---
+++ MyApp016_Sharedtask_FireBase/app/src/main/java/com/example/myapp016_sharedtask_firebase/Task.kt
@@ -0,0 +1,8 @@
+package com.example.myapp016_sharedtask_firebase
+
+data class Task(
+    val id: String, // Identifikátor úkolu (pro Firestore)
+    var name: String, // Název úkolu
+    var isCompleted: Boolean = false, // Stav dokončení
+    var assignedTo: String = "" // Kdo si úkol vzal
+)
